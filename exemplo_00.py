31c8ab5a2826c0ee8edf45331e064716870eb25b
Iasmim Horrana <horranaiasmim@gmail.com>
2024-07-29 22:40:24 -0300

    udpate-file-msg

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,13 +1,18 @@
 from loguru import logger
 
-logger.add("meu_app.log")
+#logger.add("meu_log.log")
+logger.add("meu_log.log", level="CRITICAL")
 
 #logger.info("Isso é uma mensagem informativa")
 
 def soma(x, y):
-    logger.info(x)
-    logger.info(y)
-    logger.info(x + y)
-    return x + y
-soma(2,3)
+    try:
+        soma = x + y
+        logger.info(f"Você digitou corretamente!{soma}")
+        return soma
+    except:
+        logger.critical("Você precisa digitar valores corretos.")
 
+soma(2,3)
+soma(2,7)
+soma(2,"3")
